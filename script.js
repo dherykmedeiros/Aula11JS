1df751f0157aece2e24d23f79d7eea241d55be8b
Alfredo Dheryk <dheryk@gmail.com>
2024-08-29 12:13:46 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Q01
+// const arr = [50,45,67,89,10,5]
+
+const tela = document.getElementById("tela");
+// const dobroArr = arr.map(numero => numero*2);
+
+// tela.innerHTML = dobroArr
+
+// Q02
+// arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// const pares = arr.filter(numero => numero%2 === 0)
+// tela.innerHTML = pares
+
+// Q03
+// const arr = [1,2,3,4,5,6]
+// const soma = arr.reduce((somar,numero) => somar + numero, 0);
+
+// tela.innerHTML = soma
+
+// Q04
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// const quadrado = arr.map(numero => numero**2);
+
+// tela.innerHTML = quadrado
+
+// Q05
+// const arr = ['maça','banana','goiaba']
+// const maiusculo = arr.map(nome => nome.toUpperCase())
+
+// tela.innerHTML = maiusculo
+
+// Q06 
+// const maisDeCinco = arr.filter(nome => nome.length > 5)
+
+// tela.innerHTML = maisDeCinco
+
+
